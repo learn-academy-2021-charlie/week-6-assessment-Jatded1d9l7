2c5b44b41a89321d266a97248ac35e71862789c0
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-08-13 23:28:11 -0400

    working through code-challenges

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,60 +1,109 @@
 // ASSESSMENT 6: JavaScript Coding Practical Questions with Jest
 
-// Please read all questions thoroughly
-// Pseudo coding is REQUIRED
-// If you get stuck, please leave comments to help us understand your thought process
-
-// Add appropriate dependencies to the repository:
-// $ yarn add jest
-
-// Use test driven development to complete the following questions
-// Run the file with the following command:
-// $ yarn jest
-
-// Reminder: The test will call your function
+// const { toInteger } = require("lodash")
+// const { array } = require("prop-types")
 
 
 // --------------------1) Create a function that takes in an array of objects and returns an array with a sentence about each person with their names capitalized.
-
 // a) Create a test with an expect statement using the variable provided.
-
-var people = [
-  { name: "ford prefect", occupation: "hitchhiker" },
-  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-  { name: "arthur dent", occupation: "radio employee" }
-]
+// b) Create the function that makes the test pass.
 // Expected output: ["Ford Prefect is a hitchhiker.", "Zaphod Beeblebrox is a president of the galaxy.", "Arthur Dent is a radio employee."]
 
+// ◢♂◣◥♀◤◢♂◣◥♀◤ Pseudo Code Start ◢♂◣◥♀◤◢♂◣◥♀◤
+
+// Function that takes in an array 
+// The array contains key/value pairs = name: "", occupation: ""
+// Returns an array 
+// The returned array is a sentence  
+// The sentence contains each persons
+// The sentence also contains each persons occupation  
+// Each persons name is Capitalized 
+
+// { name: "ford prefect", occupation: "hitchhiker" }
+// `${name}'s occupation is ${occupation}
+// Making a sentence out of an array of objects
+// people.name 
+// people.occupation
+
+// ◢♂◣◥♀◤◢♂◣◥♀◤ Pseudo Code END ◢♂◣◥♀◤◢♂◣◥♀◤
 
 
-// b) Create the function that makes the test pass.
+
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
+
+// var people = [
+//   { name: "ford prefect", occupation: "hitchhiker" },
+//   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+//   { name: "arthur dent", occupation: "radio employee" }
+// ]
+
+// const nameOcc = (array) => {
+//   return array.map(value => {
+//     return `${value.name}'s occupation is ${value.occupation}.`
+//   })
+// }
+// console.log(nameOcc(people))
+
+
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK END     -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
+
+
+
+
+
+
 
 
 
 // --------------------2) Create a function that takes in a mixed data array and returns an array of only the REMAINDERS of the numbers when divided by 3.
-
 // a) Create a test with an expect statement using the variables provided.
+// b) Create the function that makes the test pass.
+
+// Function that takes in mixed data array
+// returns an array 
+// New array only contains remainders of numbers when...
+// ...divided by 3
+// iterate through the array
+// filter non-numeric values
+// Use modulo to report remainer of the numbers
+
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
 
 var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
 // Expected output: [ 2, 1, -1 ]
 
+const remDiv3 = (array) => {
+  if (typeof(value) === 'number'){
+    return value
+  }
+}
+
+console.log(remDiv3(hodgepodge1))
 
 
-// b) Create the function that makes the test pass.
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK END     -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
+
 
 
 
 // --------------------3) Create a function that takes in an array of numbers and returns the sum of all the numbers cubed.
-
 // a) Create a test with an expect statement using the variables provided.
+// b) Create the function that makes the test pass.
+
+
+
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
+const numSumCube = array => {
+  array.map(**3)
+}
 
 
-// b) Create the function that makes the test pass.
+// Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK END     -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
---
+++ scratch.js
@@ -0,0 +1,11 @@
+// Instructions
+// Please read all questions thoroughly
+// Pseudo coding is REQUIRED
+// If you get stuck, please leave comments to help us understand your thought process
+// Add appropriate dependencies to the repository:
+// $ yarn add jest
+// Use test driven development to complete the following questions
+// Run the file with the following command:
+// $ yarn jest
+// Reminder: The test will call your function
+
