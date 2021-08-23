892e92f9258e3564c54ec8f138712fdf206a6daa
Jared Thomas <85851208+Jatded1d9l7@users.noreply.github.com>
2021-08-23 13:35:46 -0400

    update

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -31,18 +31,39 @@
 
 // Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
 
-// var people = [
-//   { name: "ford prefect", occupation: "hitchhiker" },
-//   { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
-//   { name: "arthur dent", occupation: "radio employee" }
-// ]
 
-// const nameOcc = (array) => {
-//   return array.map(value => {
-//     return `${value.name}'s occupation is ${value.occupation}.`
-//   })
-// }
-// console.log(nameOcc(people))
+// --QUESTION for Sarah or Austin--
+// Not sure how to run a test on objects. I am guessing there is a syntax error.
+// I am expecting the variable people to return:
+// "Ford Perfect's occupation is a hitchhiker"
+// "Zaphod Beeblebrox's occupation is president of the galaxy" 
+// "Arthur Dent's occupation is raidio employee"
+
+describe("hello", () => {
+  test("returns a sentence with each name capiitalizaed", () => {
+    expect(people()).toEqual(" [
+      "ford prefect's occupation is hitchhiker.",
+      "zaphod beeblebrox's occupation is president of the galaxy.",
+      "arthur dent's occupation is radio employee."
+    ]")
+  })
+})
+
+var people = [
+  { name: "ford prefect", occupation: "hitchhiker" },
+  { name: "zaphod beeblebrox", occupation: "president of the galaxy" },
+  { name: "arthur dent", occupation: "radio employee" }
+]
+
+  const nameOcc = (array) => {
+  return array.map(value => {
+    return `${value.name}'s occupation is ${value.occupation}.`
+  })
+}
+console.log(nameOcc(people))
+
+// // --QUESTION for Sarah or Austin--
+// I was successful returning the concatnated sentence but was unable to capitalize the names. I suspect that I would have to call on the key name and use toUpperCase for the index.
 
 
 // Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK END     -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
@@ -69,6 +90,14 @@
 
 // Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
 
+describe("hello", () => {
+  test("Returns an array of only Remainders when divided by 3", () => {
+    expect(hodgepodge1()).toEqual([2, 0 , -1, 0])
+    expect(hodgepodge2()).toEqual([2, 1, -1])
+
+  })
+})
+
 var hodgepodge1 = [23, "Heyyyy!", 45, -10, 0, "Yo", false]
 // Expected output: [ 2, 0, -1, 0 ]
 var hodgepodge2 = [5, "Hola", 43, -34, "greetings", true]
@@ -93,17 +122,40 @@ console.log(remDiv3(hodgepodge1))
 // b) Create the function that makes the test pass.
 
 
+// ◢♂◣◥♀◤◢♂◣◥♀◤ Pseudo Code Start ◢♂◣◥♀◤◢♂◣◥♀◤
+
+// Function that takes in an array of numbers
+// ex [1,2,3]
+// returns the sum of all the numbers cubed
+// iterate through the array adding each number
+// 1**3 + 2**3 + 3**3 = 99
+// cube each number first
+// then add each of the cubed numbers
+
+
+// ◢♂◣◥♀◤◢♂◣◥♀◤ Pseudo Code END ◢♂◣◥♀◤◢♂◣◥♀◤
 
 // Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK START   -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
+
+describe("hello", () => {
+  test("Returns an array of only Remainders when divided by 3", () => {
+    expect(cubeAndSum1()).toEqual(99)
+    expect(cubeAndSum2()).toEqual([1125])
+
+  })
+})
 
 var cubeAndSum1 = [2, 3, 4]
 // Expected output: 99
 var cubeAndSum2 = [0, 5, 10]
 // Expected output: 1125
 
-const numSumCube = array => {
-  array.map(**3)
+const cubeAndSum = array => {
+let cubedArray = array.map(value =>{
+  return array **3 
+})
 }
+console.log(cubeAndSum(cubeAndSum1))
 
 
 // Ƹ̵̡Ӝ̵̨̄Ʒ ▬▬ι═══════ﺤ    CODE BLOCK END     -═══════ι▬▬ Ƹ̵̡Ӝ̵̨̄Ʒ // 
--- interview-questions.md
+++ interview-questions.md
@@ -7,23 +7,32 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. As a developer, I am creating an API with a model called Animal that has_many Sightings, but OOPS! I forgot to add the foreign key. How can I fix this mistake? What is the name of the foreign key? Would the foreign key be part of the Animal model or the Sightings model?
 
-  Your answer:
+API-
+Foreign Key- 
 
-  Researched answer:
+  Your answer: A foreign key is where values in one table must match the values in another table.
+
+  Researched answer: A foreign keyu is a column in a relational database that links tow tables together and establishing a link between them. 
 
 
 
 2. Which RESTful API routes must always be passed params? Why?
 
-  Your answer:
+  Your answer: Rest= Representational State Transfer 
 
-  Researched answer:
+  Researched answer: Params are used to add more information to a program by passing a value into the route
 
 
 
 3. Name three rails generator commands. What is created by each?
 
-  Your answer:
+  Your answer: Generate commands allow passing information through the command line.
+
+  Rails generate model- allows to creade a new model. The name of the model is uppercase and singular. This allows us to create the collumns of the table. 
+
+  Rails generate migration- migration is a file that allows to make changes to a database. 
+  
+  rails generate controler Main - generates a new controller. The controller mediates information between the user, views, and the model.
 
   Researched answer:
 
